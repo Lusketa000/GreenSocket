d207101ac773cc9cac32141ad6955d450cdaa79a
Felipe1885 <felipe.leonardi2005@gmail.com>
2026-03-26 11:20:57 -0300

    mudanças no código de estado do dispositivo

--- deviceState.cpp
+++ deviceState.cpp
@@ -1,20 +1,27 @@
-// código de teste!!!! cuidado!!!! 
+// código de teste!!!! cuidado!!!! ⚠️⚠️☢️⚠️⚠️ 
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WebServer.h>
 
-enum DeviceState {
-    ACTIVE,
-    STANDBY
+bool isDeviceActive = false;
+
+float current() {
+    // Função para capturar o valor de corrente do dispositivo
+    // Substituir esta função pela que captura o valor de corrente do dispositivo
+    return 0.9;
 };
 
-const float currentThreshold = 0.5;
+float currentThreshold() {
+    // Função para capturar o limiar do dispositivo
+    // Substituir esta função pela que captura o limiar do dispositivo
+    return 0.5;
+};
 
-DeviceState deviceState(float current) {
-    if (current > currentThreshold) {
-        return ACTIVE;
+bool deviceState(float current) {
+    if (current > currentThreshold()) {
+        return isDeviceActive = true;
     } else {
-        return STANDBY;
+        return isDeviceActive = false;
     }
 }
 
@@ -22,14 +29,12 @@ void setup() {
     Serial.begin(115200);
 }
 
-void loop() {
-    float current = 0.9; //alguma corrente que a gente vai ler
-    
-    DeviceState state = deviceState(current);
-    if (state == ACTIVE) {
+void loop() {    
+    bool isActive = deviceState(current());
+    if (!isActive) {
         Serial.println("Dispositivo está ligado");
     } else {
-        Serial.println("Dispositivo está em desligado, mas consumindo energia");
+        Serial.println("Dispositivo em standby");
     }
 
     delay(1000);
